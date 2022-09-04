6a34e015b37409e714d5207869dc2a0b831a4ce2
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-09-04 21:28:50 +0700

    fix: change import folder

--- apps/web-nextjs/next.config.js
+++ apps/web-nextjs/next.config.js
@@ -1,13 +1,23 @@
+// const ampValidator = require("amphtml-validator");
+
 const withTM = require("next-transpile-modules")(["ui"]);
 
 const IS_DEV = process.env.NODE_ENV !== "production";
 
 const config = {
   reactStrictMode: true,
-  // swcMinify: true,
+  swcMinify: true,
   // experimental: {
   //   forceSwcTransforms: true
   // }
+  // amp: {
+  //   validator: ampValidator
+  // },
+  experimental: {
+    amp: {
+      skipValidation: true
+    }
+  }
 };
 
 module.exports = withTM(config);
--- apps/web-nextjs/src/pages/index.tsx
+++ apps/web-nextjs/src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { useTheme } from 'ui/hooks'
 import { Typography } from 'ui/components'
 import React from "react";
+import { useAmp } from 'next/amp';
+
 
 const { Text, Title } = Typography
 export default function Web() {
-
+  
   const { theme } = useTheme()
-
+  const isAmp = useAmp()
+  
   return (
     <React.Fragment>
       <Title>{theme.colors.primary}</Title>
@@ -14,3 +17,5 @@ export default function Web() {
     </React.Fragment>
   );
 }
+
+export const config = { amp: true }
