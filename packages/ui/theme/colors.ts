a3933dccd90cdc0993d2f5af06f18f590e49d327
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-09-08 23:40:02 +0700

    feat: add danger color

--- packages/ui/theme/colors.ts
+++ packages/ui/theme/colors.ts
@@ -5,6 +5,7 @@ const colors = {
 
   // main colors
   primary: '#009AF9',
+  danger: '#FF0000',
   onSurface: 'rgba(255, 255, 255, 0.15);',
   surface: '#111111',
   disabled: 'rgba(255, 255, 255, 0.3)',
